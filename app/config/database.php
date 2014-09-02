19db26a0e3c55b7ccd0433975a2483ff98da1cc3
Caroline Vu <caroline.vu@student.uibk.ac.at>
2014-09-02 11:17:40 +0200

    optimized for server: spa api for profile and registration made. correction of cors headers

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'database', 
-			'username'  => 'root', 
-			'password'  => 'root', 
+			'database'  => 'kakadu', 
+			'username'  => 'kakadu', 
+			'password'  => 'kakadu', 
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
